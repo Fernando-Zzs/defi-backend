bfe149fb0227d83bec28638f0334dc842599ccbc
Fernando-Zzs <65518148+Fernando-Zzs@users.noreply.github.com>
2024-10-11 22:35:36 +0800

    project init

---
+++ src/main/java/com/example/defi_backend/DefiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.defi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefiBackendApplication.class, args);
+    }
+
+}
